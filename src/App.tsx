7dc994346461515bd25b49fdd6de73cd965d9204
Liudmyla_Taran <taran.lyudok@gmail.com>
2025-10-16 14:58:08 +0300

    add lazyLoad

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,41 @@
 import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
 import "./App.css";
 
-import MainPage from "./pages/MainPage/MainPage";
 import MainLayout from "./components/MainLayout/MainLayout";
-import NotFoumdPage from "./pages/NotFoumdPage/NotFoumdPage";
-import NanniesPage from "./pages/NanniesPage/NanniesPage";
 import { ToastContainer } from "react-toastify";
-import FavoritesPage from "./pages/FavoritesPage/FavoritesPage";
 import useCurrentUser from "./hooks/useCurrentUser";
 import PrivateRoute from "./PrivateRoute";
+
+const MainPage = lazy(() => import("./pages/MainPage/MainPage"));
+const NanniesPage = lazy(() => import("./pages/NanniesPage/NanniesPage"));
+const FavoritesPage = lazy(() => import("./pages/FavoritesPage/FavoritesPage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoumdPage/NotFoumdPage"));
 
 function App() {
   const { data: user } = useCurrentUser();
   return (
     <>
-      <Routes>
-        <Route element={<MainLayout fullWidth={true} />}>
-          <Route path="/" element={<MainPage />} />
-        </Route>
-        <Route element={<MainLayout />}>
-          <Route path="/nannies" element={<NanniesPage />} />
-          <Route
-            path="/favorites"
-            element={
-              <PrivateRoute isAuth={!!user}>
-                <FavoritesPage />
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<NotFoumdPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route element={<MainLayout fullWidth={true} />}>
+            <Route path="/" element={<MainPage />} />
+          </Route>
+          <Route element={<MainLayout />}>
+            <Route path="/nannies" element={<NanniesPage />} />
+            <Route
+              path="/favorites"
+              element={
+                <PrivateRoute isAuth={!!user}>
+                  <FavoritesPage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
       <ToastContainer position="top-right" autoClose={2000} />
     </>
   );
